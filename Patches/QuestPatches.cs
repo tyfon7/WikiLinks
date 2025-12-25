f9c1f3be55efb5e9dc9e06a216e1af7cd84032af
Tyfon <29051038+tyfon7@users.noreply.github.com>
2025-12-25 12:16:41 -0800

    move to description block to support unaccepted quests

--- Patches/QuestPatches.cs
+++ Patches/QuestPatches.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using EFT.Quests;
 using EFT.UI;
 using HarmonyLib;
 using SPT.Reflection.Patching;
@@ -14,70 +13,92 @@ public static class QuestPatches
 
     public static void Enable()
     {
-        new QuestObjectivesViewPatch().Enable();
+        new NotesTaskDescriptionPatch().Enable();
+        new NotesTaskDescriptionShortPatch().Enable();
     }
 
-    public class QuestObjectivesViewPatch : ModulePatch
+    public class NotesTaskDescriptionPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
         {
-            return AccessTools.DeclaredMethod(typeof(QuestObjectivesView), nameof(QuestObjectivesView.Show)).MakeGenericMethod([typeof(QuestClass)]);
+            return AccessTools.DeclaredMethod(typeof(NotesTaskDescription), nameof(NotesTaskDescription.Show));
         }
 
         [PatchPostfix]
-        public static void Postfix(QuestObjectivesView __instance, IConditional conditional)
+        public static void Postfix(UIElement __instance, QuestClass quest)
+        {
+            var parent = __instance.transform.Find("Center/Scrollview/Content/CenterBlock/DescriptionBlock");
+            CreateButton(quest, __instance, parent);
+        }
+    }
+
+    public class NotesTaskDescriptionShortPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.DeclaredMethod(typeof(NotesTaskDescriptionShort), nameof(NotesTaskDescriptionShort.Show)).MakeGenericMethod([typeof(QuestClass), typeof(AbstractQuestControllerClass)]);
+        }
+
+        [PatchPostfix]
+        public static void Postfix(UIElement __instance, object conditional)
         {
             if (conditional is not QuestClass quest)
             {
                 return;
             }
 
-            SimpleContextMenuButton openWikiButton = null;
-
-            var existing = __instance.transform.Find("OpenWikiButton");
-            if (existing != null)
-            {
-                openWikiButton = existing.GetComponent<SimpleContextMenuButton>();
-            }
-
-            if (!Settings.EnableQuestButton.Value)
-            {
-                if (openWikiButton != null)
-                {
-                    openWikiButton.Dispose();
-                    Object.Destroy(openWikiButton);
-                }
-
-                return;
-            }
-
-            if (openWikiButton == null)
-            {
-                // Find a button to clone
-                ButtonTemplate ??= ItemUiContext.Instance.ContextMenu.transform.Find("InteractionButtonsContainer/Button Template")?.GetComponent<SimpleContextMenuButton>();
-
-                openWikiButton = UnityEngine.Object.Instantiate<SimpleContextMenuButton>(ButtonTemplate, __instance.transform);
-                openWikiButton.name = "OpenWikiButton";
-                openWikiButton.transform.SetSiblingIndex(1); // After label so it's on top
-
-                // This is needed or the inner elements all collapse
-                var fitter = openWikiButton.GetOrAddComponent<ContentSizeFitter>();
-                fitter.horizontalFit = fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
-
-                var layout = openWikiButton.GetComponent<LayoutElement>();
-                layout.ignoreLayout = true;
-
-                // Position on right
-                var rect = openWikiButton.RectTransform();
-                rect.pivot = new(1, 1);
-                rect.anchorMin = rect.anchorMax = new(1, 1);
-            }
-
-            // I know this isn't how you translate things, but it's good enough
-            var text = $"{"OPEN".Localized()} WIKI";
-            openWikiButton.Show(text, text, CacheResourcesPopAbstractClass.Pop<Sprite>("Characteristics/Icons/Inspect"), () => Url.OpenWiki(quest.Id), () => { });
-
-            __instance.AddDisposable(openWikiButton.Close);
+            var parent = __instance.transform.Find("Description");
+            CreateButton(quest, __instance, parent);
         }
+    }
+
+    private static void CreateButton(QuestClass quest, UIElement element, Transform parent)
+    {
+        SimpleContextMenuButton openWikiButton = null;
+
+        var existing = parent.Find("OpenWikiButton");
+        if (existing != null)
+        {
+            openWikiButton = existing.GetComponent<SimpleContextMenuButton>();
+        }
+
+        if (!Settings.EnableQuestButton.Value)
+        {
+            if (openWikiButton != null)
+            {
+                openWikiButton.Dispose();
+                Object.Destroy(openWikiButton);
+            }
+
+            return;
+        }
+
+        if (openWikiButton == null)
+        {
+            // Find a button to clone
+            ButtonTemplate ??= ItemUiContext.Instance.ContextMenu.transform.Find("InteractionButtonsContainer/Button Template")?.GetComponent<SimpleContextMenuButton>();
+
+            openWikiButton = UnityEngine.Object.Instantiate<SimpleContextMenuButton>(ButtonTemplate, parent);
+            openWikiButton.name = "OpenWikiButton";
+
+            // This is needed or the inner elements all collapse
+            var fitter = openWikiButton.GetOrAddComponent<ContentSizeFitter>();
+            fitter.horizontalFit = fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+
+            var layout = openWikiButton.GetComponent<LayoutElement>();
+            layout.ignoreLayout = true;
+
+            // Position on bottom right
+            var rect = openWikiButton.RectTransform();
+            rect.pivot = new(1, 0);
+            rect.anchorMin = rect.anchorMax = new(1, 0);
+            rect.anchoredPosition = new(-20, -30);
+        }
+
+        // I know this isn't how you translate things, but it's good enough
+        var text = $"{"OPEN".Localized()} WIKI";
+        openWikiButton.Show(text, text, CacheResourcesPopAbstractClass.Pop<Sprite>("Characteristics/Icons/Inspect"), () => Url.OpenWiki(quest.Id), () => { });
+
+        element.AddDisposable(openWikiButton.Close);
     }
 }
